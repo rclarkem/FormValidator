a16bbc1bf350b25ed6f88bae6e162e7bc54450fe
Reina <r.clarkemitchell@gmail.com>
2020-10-16 21:30:39 -0400

    added in email  validator and refactored

--- index.js
+++ index.js
@@ -6,22 +6,31 @@ const confirmPassword = document.querySelector('#confirm-password');
 
 function showError(element, message) {
   const formControl = element.parentElement;
-  formControl.className = formControl.className + ' error';
+  formControl.className = 'form-control error';
   const errorMess = formControl.children[2];
   errorMess.textContent = message;
 }
 
 function showSuccess(element) {
   const formControl = element.parentElement;
-  formControl.className = formControl.className + ' success';
+  formControl.className = 'form-control success';
+}
+
+function validEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(String(email.value.trim()))) {
+    return showSuccess(email);
+  } else {
+    return showError(email, 'Email not Valid');
+  }
 }
 
 function checkRequired(inputs) {
-  inputs.forEach((input) => {
+  return inputs.forEach((input) => {
     if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
+      return showError(input, `${getFieldName(input)} is required`);
     } else {
-      showSuccess(input);
+      return showSuccess(input);
     }
   });
 }
@@ -30,7 +39,22 @@ function getFieldName(input) {
   return input.id.slice(0, 1).toUpperCase() + input.id.slice(1).toLowerCase();
 }
 
+function checkLength(input, startLength, endlength) {
+  console.log(input.value.length);
+  if (input.value.length < startLength) {
+    return showError(input, `${getFieldName(input)} must be at least ${startLength} characters`);
+  } else if (input.value.length > endlength) {
+    return showError(input, `${getFieldName(input)} must not exceed ${endlength} characters`);
+  } else {
+    return showSuccess(input);
+  }
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
   checkRequired([username, email, password, confirmPassword]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  validEmail(email);
 });
